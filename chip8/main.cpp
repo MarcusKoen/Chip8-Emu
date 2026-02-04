eb0b014acbdc1351330fbeea228791909c2dee81
MarcusKoen <37168149+MarcusKoen@users.noreply.github.com>
2026-02-04 20:46:41 +0200

    Need a break

--- chip8/main.cpp
+++ chip8/main.cpp
@@ -28,6 +28,8 @@ public:
 void Chip8::execute()
 {
 
+    pc += 2;
+
     switch(opcode)
     {
 ////////////////////////////////CLS - TODO pattern
@@ -119,18 +121,141 @@ void Chip8::execute()
 
         V[x]=kk;
 
+
         cout << "V" << x << " = " << hex << kk << endl;
     }
 
-//////////////////////////////////////////ADD Vx, byte
+////////////////////////////////////////7//ADD Vx, byte
 
 
+    if ((opcode & 0xF000)== 0x7000)
+    {
+        auto x = (opcode & 0x0F00) >> 8;
+        auto kk = opcode & 0x00FF;
+
+        V[x] = V[x] + kk;
+
+        cout << " ADD Vx, byte";
+
+    }
+
+//////////////////////////////////// 8xy0 - LD Vx, Vy Vx=Vy
+
+    if (((opcode & 0xF000)==0x8000) && ((opcode & 0x000F)==0) )
+    {
+        auto x = (opcode & 0x0F00) >> 8;
+        auto y = (opcode & 0x00F0) >> 4;
+
+        V[x] = V[y];
+
+        cout << "8xy0 - LD Vx" << endl;
+    }
+
+
+///////////////////////////////// 8xy1 - OR Vx, Vy
+
+    if (((opcode & 0xF000)==0x8000)&&(opcode & 0x000F)==1)
+    {
+
+        auto x = (opcode & 0x0F00) >> 8;
+        auto y = (opcode & 0x00F0) >> 4;
+
+        V[x] = V[x] |  V[y];
+
+        cout << "8xy1 - OR Vx, Vy " << endl;
+    }
+//////////////////////////////// 8xy2 - AND Vx, Vy
+
+    if (((opcode & 0xF000)==0x8000)&&(opcode & 0x000F)==2)
+    {
+
+        auto x = (opcode & 0x0F00) >> 8;
+        auto y = (opcode & 0x00F0) >> 4;
+
+        V[x] = V[x] &  V[y];
+
+        cout << "8xy2 - AND Vx, Vy" << endl;
+    }
+////////////////////////////// 8xy3 - XOR Vx, Vy
+
+    if (((opcode & 0xF000)==0x8000)&&(opcode & 0x000F)==3)
+    {
+
+        auto x = (opcode & 0x0F00) >> 8;
+        auto y = (opcode & 0x00F0) >> 4;
+
+        V[x] = V[x] ^ V[y];
+
+        cout << "8xy3 - XOR Vx, Vy" << endl;
+    }
+//////////////////////////////// 8xy4 - ADD Vx, Vy
+    if (((opcode & 0xF000)==0x8000)&&(opcode & 0x000F)==4)
+    {
+
+        auto x = (opcode & 0x0F00) >> 8;
+        auto y = (opcode & 0x00F0) >> 4;
+
+        V[x] = V[x] + V[y];//TODO VF flag
+
+        cout << "8xy4 - ADD Vx, Vy" << endl;
+    }
+    //TODO all 8xy567E
+
+///////////////////////////////////////// 9xy0 - SNE Vx, Vy
+    if (((opcode & 0xF000)==0x9000)&&(opcode & 0x000F)==0)
+    {
+
+        auto x = (opcode & 0x0F00) >> 8;
+        auto y = (opcode & 0x00F0) >> 4;
+
+        if (V[x] != V[y])
+        {
+            pc +=2;//skip instruction
+        }
+        cout << "9xy0 - SNE Vx, Vy" << endl;
+    }
+
+    //TODO Annn
+///////////////////////////////////////// Bnnn - JP V0, addr
+
+    if ((opcode & 0xF000)==0xB000)
+    {
+
+        auto nnn = opcode & 0x0FFF;
+
+        pc = nnn + V[0];
+
+        cout << "Bnnn - JP V0, addr" << endl;
+    }
+
+
+
+}
+
+void Chip8::cycle()
+{
+    // Fetch
+    opcode = (memory[pc] << 8) | memory[pc + 1];
+
+    // Decode + Execute
+    execute();
+}
+
+Chip8::Chip8()
+{
+    pc = 0x200;   // CHIP-8 programs start here
+    sp = 0;
+    opcode = 0;
+
+    memset(memory, 0, sizeof(memory));
+    memset(V, 0, sizeof(V));
+    memset(stack, 0, sizeof(stack));
 }
 
 int main()
 {
     Chip8 emu;
-    emu.opcode = 0x6244;
+    emu.opcode = 0xB230;
     emu.execute();
 
     return 0;
